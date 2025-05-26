bbf8ddd5372b3e7a56eca7a4085d8b35fd52cc8e
Leones Silva de Oliveira <50273318+leonesoliveira@users.noreply.github.com>
2025-05-26 17:03:15 -0300

    deploy completed

---
+++ app/config/log.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'host' => "127.0.0.1", 
+    'user' => "leones", 
+    'pass' => "leones", 
+    'type' => "mysql", 
+    'port' => "3306", 
+    'name' => "vitaflow_log", 
+    'prep' => "1", 
+    'slog' => "SystemSqlLogService", 
+];
---
+++ app/config/permission.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'host' => "127.0.0.1", 
+    'user' => "leones", 
+    'pass' => "leones", 
+    'type' => "mysql", 
+    'port' => "3306", 
+    'name' => "vitaflow", 
+    'prep' => "1", 
+    'slog' => "SystemSqlLogService", 
+];
